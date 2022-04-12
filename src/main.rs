4001fdc3db68b89df1f91593e3b933994268463e
Andreas Maier <andreas.martin.maier@desy.de>
2022-04-12 19:41:34 +0200

    short readme

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+//! An *ev*ent *il*lustrator for Monte Carlo collider events.
+//!
+//! # How to use
+//!
+//! You can also find precompiled executables on
+//! [github](https://github.com/a-maier/evil/releases). Start with
+//!
+//!   evil EVENTFILE
+//!
+//! The event file should be in the LHEF or version 2 of the HepMC
+//! format and can be compressed (bzip2, gzip, lz4, zstd).
+//!
+//! If [Rust and Cargo](https://www.rust-lang.org/) are installed on
+//! your system, you can of course compile and run directly from the
+//! source code:
+//!
+//!   cargo run --release -- EVENTFILE
+//!
 mod app;
 mod auto_decompress;
 mod event;
